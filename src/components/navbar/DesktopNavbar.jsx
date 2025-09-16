8fb327512030d2f6ee538188c07e465da368b27c
Ediw7 <ediwicoro26@gmail.com>
2025-09-16 14:26:57 +0700

    fix: hapus search bar pada nvabar desktop

--- src/components/navbar/DesktopNavbar.jsx
+++ src/components/navbar/DesktopNavbar.jsx
@@ -50,22 +50,7 @@ export default function DesktopNavbar({ currentPage, onNavigate }) {
               </button>
             ))}
           </div>
-
-          {/* Search */}
-          <div className="flex items-center space-x-4">
-            <div className="relative">
-              <input
-                type="text"
-                placeholder="Cari resep..."
-                className="w-72 pl-11 pr-4 py-3 bg-gray-50 border border-gray-200 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent focus:outline-none transition-all duration-200 placeholder-slate-500"
-              />
-              <div className="absolute inset-y-0 left-0 pl-3 flex items-center">
-                <svg className="h-5 w-5 text-blue-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
-              </div>
-            </div>
-          </div>
+         
         </div>
       </div>
     </nav>
